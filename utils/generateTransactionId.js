a633005baf1f0507739bf3feda493c85fa6baf64
Dishan D <168318955+Dishan-D@users.noreply.github.com>
2025-11-16 16:37:02 +0530

    Revert "Feature/perf caching"

--- utils/generateTransactionId.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-
-function generateTransactionId(prefix = '') {
-  return prefix + crypto.randomBytes(6).toString('hex');
-}
-
-module.exports = generateTransactionId;
